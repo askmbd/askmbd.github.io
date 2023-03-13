9a620ae5e50b35401f1ed87599c57b7e4093f861
askmbd <127472873+askmbd@users.noreply.github.com>
2023-03-13 15:16:10 +0600

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -100,18 +100,17 @@ prevNextIcon.forEach((icon) => {
 
 
 
-document.addEventListener("DOMContentLoaded", function(){
-  window.addEventListener('scroll', function() {
-      if (window.scrollY > 50) {
-        document.getElementById('navbar_top').classList.add('fixed-top');
-        // add padding top to show content behind navbar
-        navbar_height = document.querySelector('sticky-top').offsetHeight;
-        document.body.style.paddingTop = navbar_height + 'px';
-      } else {
-        document.getElementById('navbar_top').classList.remove('fixed-top');
-         // remove padding top from body
-        document.body.style.paddingTop = '0';
-      } 
-  });
-}); 
+<script>
+window.onscroll = function() {myFunction()};
 
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky-top")
+  } else {
+    navbar.classList.remove("sticky-top");
+  }
+}
+</script>
