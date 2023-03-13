36ea1ab7a7a53fb69e71bc0d4570157e2bb6570d
askmbd <127472873+askmbd@users.noreply.github.com>
2023-03-13 15:12:08 +0600

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -105,7 +105,7 @@ document.addEventListener("DOMContentLoaded", function(){
       if (window.scrollY > 50) {
         document.getElementById('navbar_top').classList.add('fixed-top');
         // add padding top to show content behind navbar
-        navbar_height = document.querySelector('.navbar').offsetHeight;
+        navbar_height = document.querySelector('sticky-top').offsetHeight;
         document.body.style.paddingTop = navbar_height + 'px';
       } else {
         document.getElementById('navbar_top').classList.remove('fixed-top');
