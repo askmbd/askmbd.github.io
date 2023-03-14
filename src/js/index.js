d50c25b5b77e2c17b3af7289e64fca785abb001b
askmbd <127472873+askmbd@users.noreply.github.com>
2023-03-14 10:14:32 +0600

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -110,9 +110,9 @@ prevNextIcon.forEach((icon) => {
                // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
                function myFunction() {
                if (window.pageYOffset >= sticky) {
-                  navbar.classList.add("sticky")
+                  navbar.classList.add("sticky-top")
                } else {
-                  navbar.classList.remove("sticky");
+                  navbar.classList.remove("sticky-top");
                }
                }
       
